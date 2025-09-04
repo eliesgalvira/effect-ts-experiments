a405c0560899faf7078a13d5f26d4b7ceedc4046
Elies <68759092+alias313@users.noreply.github.com>
2025-09-05 00:07:55 +0200

    Hear me out

--- src/index.ts
+++ src/index.ts
@@ -9,39 +9,32 @@ class Pokemon extends Schema.Class<Pokemon>("Pokemon")({
     height: Schema.Number,
     weight: Schema.Number,
 }) {};
+
 class FetchError extends Data.TaggedError("FetchError")<{ customMessage: string }> {};
 
 class JsonError extends Data.TaggedError("JsonError")<{ customMessage: string }> {};
 
-/// Effect<Response, FetchError>
-const fetchRequest = (baseUrl: string) => Effect.tryPromise({
-    try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
-    catch: () => new FetchError({ customMessage: "There was an error fetching the data" })
-});
-  
-/// Effect<unknown, JsonError>
-const jsonResponse = (response: Response) => Effect.tryPromise({
-    try: () => response.json(),
-    catch: () => new JsonError({ customMessage: "There was an error parsing the data" })
-});
-
-/// Effect<unknown, ParseError>
-const decodePokemon = Schema.decodeUnknown(Pokemon);
-
-/// Effect<unknown, JsonError | FetchError | ParseError>
-const program = Effect.gen(function* () {
+const getPokemon = Effect.gen(function* () {
     const baseUrl = yield* config;
-    const response = yield* fetchRequest(baseUrl);
+
+    const response = yield* Effect.tryPromise({
+        try: () => fetch(`${baseUrl}/api/v2/pokemon/garchomp/`),
+        catch: () => new FetchError({ customMessage: "There was an error fetching the data" })
+    });
+
     if (!response.ok) {
         return yield* new FetchError({ customMessage: "There was an error fetching the data" });
     }
 
-    const json = yield* jsonResponse(response);
-    return yield* decodePokemon(json);
+    const json = yield* Effect.tryPromise({
+        try: () => response.json(),
+        catch: () => new JsonError({ customMessage: "There was an error parsing the data" })
+    });
+
+    return yield* Schema.decodeUnknown(Pokemon)(json);
 });
 
-/// Effect<unknown, never>
-const main = program.pipe(
+const main = getPokemon.pipe(
     Effect.catchTags({
         FetchError: (error) => Effect.succeed<string>(error.customMessage),
         JsonError: (error) => Effect.succeed<string>(error.customMessage),
