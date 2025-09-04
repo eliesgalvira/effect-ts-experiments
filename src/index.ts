4095600842a44d99411cc1e80a3b8468be052282
Elies <68759092+alias313@users.noreply.github.com>
2025-09-04 19:02:22 +0200

    Make sure we receive the right shape of Pokemon

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-import { Console, Effect, Data } from "effect";
+import { Console, Effect, Data, Schema } from "effect";
+
+const Pokemon = Schema.Struct({
+    id: Schema.Number,
+    order: Schema.Number,
+    name: Schema.String,
+    height: Schema.Number,
+    weight: Schema.Number,
+});
+
+/// Effect<unknown, ParseError>
+const decodePokemon = Schema.decodeUnknown(Pokemon);
 
 class FetchError extends Data.TaggedError("FetchError")<
     {
@@ -24,14 +35,15 @@ const jsonResponse = (response: Response) => Effect.tryPromise({
     catch: () => new JsonError({ customMessage: "There was an error parsing the data" })
 });
 
-/// Effect<unknown, JsonError | FetchError>
+/// Effect<unknown, JsonError | FetchError | ParseError>
 const program = Effect.gen(function* () {
     const response = yield* fetchRequest;
     if (!response.ok) {
         return yield* new FetchError({ customMessage: "There was an error fetching the data" });
     }
 
-    return yield* jsonResponse(response);
+    const json = yield* jsonResponse(response);
+    return yield* decodePokemon(json);
 });
 
 /// Effect<unknown, never>
@@ -39,6 +51,7 @@ const main = program.pipe(
     Effect.catchTags({
         FetchError: (error) => Effect.succeed<string>(error.customMessage),
         JsonError: (error) => Effect.succeed<string>(error.customMessage),
+        ParseError: (error) => Effect.succeed<string>(error.message),
     }),
     Effect.tap(Console.log)
 );
