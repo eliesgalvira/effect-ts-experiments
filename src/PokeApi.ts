c331bbaa22dcb0f344e38794fedd26d2a7f6f097
Elies <68759092+alias313@users.noreply.github.com>
2025-09-12 21:20:14 +0200

    Redundant make

--- src/PokeApi.ts
+++ src/PokeApi.ts
@@ -4,36 +4,6 @@ import { Pokemon } from "./schema.ts";
 import { PokemonCollection } from "./PokemonCollection.ts";
 import { BuildPokeApiUrl } from "./BuildPokeApiUrl.ts";
 
-const make = Effect.gen(function* () {
-  const pokemonCollection = yield* PokemonCollection;
-  const buildPokeApiUrl = yield* BuildPokeApiUrl;
-
-  return {
-    getPokemon: Effect.gen(function* () {
-      const requestUrl = buildPokeApiUrl({
-        name: pokemonCollection[0],
-      });
-
-      const response = yield* Effect.tryPromise({
-        try: () => fetch(requestUrl),
-        catch: () => new FetchError({ customMessage: "Failed to fetch pokemon, promise failed" }),
-      });
-
-      if (!response.ok) {
-        return yield* new FetchError({ customMessage: "Failed to fetch pokemon, response not ok" });
-      }
-
-      const json = yield* Effect.tryPromise({
-        try: () => response.json(),
-        catch: (): JsonError => new JsonError({ customMessage: "Failed to parse pokemon" }),
-      });
-
-      return yield* Schema.decodeUnknown(Pokemon)(json);
-    }),
-  };
-});
-
-
 export class PokeApi extends Effect.Service<PokeApi>()("PokeApi", {
   effect: Effect.gen(function* () {
     const pokemonCollection = yield* PokemonCollection;
