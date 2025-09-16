4015c5218fca4fe2c4f860ceaea01cd94f7f7ba0
Elies <68759092+alias313@users.noreply.github.com>
2025-09-16 16:35:36 +0200

    Proper use of Config with Redacted

--- src/pokémon/src/PokeApiUrl.ts
+++ src/pokémon/src/PokeApiUrl.ts
@@ -1,4 +1,4 @@
-import { Config, Context, Effect, Layer } from "effect";
+import { Config, Context, Effect, Layer, Redacted } from "effect";
 
 export class PokeApiUrl extends Context.Tag("PokeApiUrl")<
   PokeApiUrl,
@@ -7,8 +7,8 @@ export class PokeApiUrl extends Context.Tag("PokeApiUrl")<
   static readonly Live = Layer.effect(
     this, 
     Effect.gen(function* () {
-        const baseUrl = yield* Config.string("BASE_URL");
-        return PokeApiUrl.of(`${baseUrl}/api/v2/pokemon`);
+        const baseUrl = yield* Config.redacted("BASE_URL");
+        return PokeApiUrl.of(`${Redacted.value(baseUrl)}/api/v2/pokemon`);
     })
   );
 }
